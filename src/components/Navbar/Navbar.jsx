2850ae6fcd05637656aa9a16acd3baab6053958f
Hariom Vashista <rishivashista4@gmail.com>
2024-01-28 01:15:48 +0530

    add vercel.json file

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -15,6 +15,7 @@ function Navbar() {
         setNavbar((prevState)=>{
             return (!prevState)
         })
+        console.log(navbar)
     }
 
   return (
@@ -40,16 +41,14 @@ function Navbar() {
         </div>
 
         {/* Mobile Navbar */}
-        <div className='md:hidden'><GiHamburgerMenu size={25} onClick={showNavbar} /></div>
+        <div className={`${navbar?"hidden":"block"} md:hidden`} onClick={showNavbar} ><GiHamburgerMenu size={25} /></div>
         <div className={`${navbar?"block":"hidden"} right-0 top-0 w-2/4 h-full fixed  bg-[#1c1c21] px-4 border-l animate-slideInRight shadow-[5px_0px_10px_10px_rgb(256,256,256)]`}>
-            <div>
-                <RxCross2 onClick={showNavbar} size={28} className='relative right-0 mt-6'/>
-            </div>
+            <div><RxCross2 size={28} className='mt-6' onClick={showNavbar}/></div>
             <div className='h-full'>
                 <ul className='flex flex-col justify-center relative -top-24 h-full'>
-                    <Link to="/about"><li className='text-xl my-4 flex items-center'><IoPersonOutline size={24} className='mr-4'/>About</li></Link>
-                    <Link to="/projects"><li className='text-xl my-4 flex items-center'><FaLaptopCode size={24} className='mr-4'/>Projects</li></Link>
-                    <Link to="contact"><li className='text-xl my-4 flex items-center'><MdOutlineMail size={24} className='mr-4'/>Contact</li></Link>
+                    <Link to="/about"><li className='text-xl my-4 flex items-center' onClick={()=>(setNavbar(false))}><IoPersonOutline size={24} className='mr-4'/>About</li></Link>
+                    <Link to="/projects"><li className='text-xl my-4 flex items-center' onClick={()=>(setNavbar(false))}><FaLaptopCode size={24} className='mr-4'/>Projects</li></Link>
+                    <Link to="contact"><li className='text-xl my-4 flex items-center' onClick={()=>(setNavbar(false))}><MdOutlineMail size={24} className='mr-4'/>Contact</li></Link>
                 </ul>
             </div>
         </div>
